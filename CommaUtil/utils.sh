7223e9d539fb83de909ca6c736626a23eb796752
tonesto7 <tonesto7@users.noreply.github.com>
2025-02-08 19:35:15 -0500

    syncing

--- CommaUtil/utils.sh
+++ CommaUtil/utils.sh
@@ -11,5 +11,13 @@ readonly UTILS_SCRIPT_VERSION="3.0.0"
 readonly UTILS_SCRIPT_MODIFIED="2025-02-09"
 
 ###############################################################################
-# Global Variables
+# Encryption/Decryption Functions
 ###############################################################################
+encrypt_credentials() {
+    local data="$1" output="$2"
+    openssl enc -aes-256-cbc -salt -pbkdf2 -in <(echo "$data") -out "$output" -pass file:/data/params/d/GithubSshKeys
+}
+decrypt_credentials() {
+    local input="$1"
+    openssl enc -d -aes-256-cbc -pbkdf2 -in "$input" -pass file:/data/params/d/GithubSshKeys
+}
